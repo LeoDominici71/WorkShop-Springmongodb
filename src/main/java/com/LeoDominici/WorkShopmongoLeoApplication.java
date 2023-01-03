657f76857472996e9949d637cec924395f967b7a
LeoDominici71 <leonardo_requena@outlook.com>
2023-01-03 02:15:06 +0000

    primeiro commit

---
+++ src/main/java/com/LeoDominici/WorkShopmongoLeoApplication.java
@@ -0,0 +1,13 @@
+package com.LeoDominici;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkShopmongoLeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkShopmongoLeoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LeoDominici/WorkShopmongoLeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LeoDominici;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkShopmongoLeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
