02565e7e61629c3e0e812f8b91739763123ac0d2
LeoDominici71 <leonardo_requena@outlook.com>
2023-01-04 03:14:26 +0000

    trocar autor por authorDTO

--- src/main/java/com/LeoDominici/Instantiation.java
+++ src/main/java/com/LeoDominici/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.LeoDominici.domain.Post;
 import com.LeoDominici.domain.User;
+import com.LeoDominici.dto.AuthorDTO;
 import com.LeoDominici.repository.PostRepository;
 import com.LeoDominici.repository.UserRepository;
 @Configuration
@@ -37,11 +38,13 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para Sao Paulo. Abracos!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje",maria);
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para Sao Paulo. Abracos!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje",new AuthorDTO(maria));
 
 		
-		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		
