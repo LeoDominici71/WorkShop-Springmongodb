4f0491756f87b3e3022f655c2158340e1562462e
LeoDominici71 <leonardo_requena@outlook.com>
2023-01-04 03:30:46 +0000

    ENDPOINT PARA RETORNAR OS POSTS DE UM USUARIO

--- src/main/java/com/LeoDominici/controler/UserControler.java
+++ src/main/java/com/LeoDominici/controler/UserControler.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.LeoDominici.domain.Post;
 import com.LeoDominici.domain.User;
 import com.LeoDominici.dto.UserDTO;
 import com.LeoDominici.services.UserService;
@@ -57,6 +58,12 @@ public class UserControler {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();  
+	}
+	
+	@RequestMapping(value="/{id}/posts",method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 	
